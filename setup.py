a9d1c34264dea40fd46828fe5287367afc0d7dfb
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-08-08 21:45:48 +0300

    asd

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup , find_namespace_packages
 
-setup(name='goit-HOMEWORK-2-module-7',
+setup(name='clean-folder',
       version='0.0.1',
       description='My Very useful code',
       author='Natrix',
       author_email='test@example.com',
       license='MyIT',
       packages=find_namespace_packages(),
-      entry_points={'console_scripts': ['greeting=goit-HOMEWORK-2-module-7.main:greeting']},
-#greeting - команда яка повинна виконатись в терміналі
+      entry_points={'console_scripts': ['clean-folder=goit-HOMEWORK-2-module-7.clean:main']},
+#clean - команда яка повинна виконатись в терміналі
 #після "=" пишемо шлях до файлу де знаходиться функція => goit-HOMEWORK-2-module-7.main
-#після ":" пишемо назву функції greeting
-      classifiers = [
+#після ":" пишемо назву функції normalize, process_folder
+      classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
